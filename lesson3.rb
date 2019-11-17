7a9b6c88da2261155e22bc4566e79586584cce95
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2019-11-17 14:19:37 +0900

    [Finish]chapter2-4

---
+++ lesson3.rb
@@ -0,0 +1,7 @@
+# ctrl + b
+puts "webcamp".upcase
+puts "WEBCAMP".downcase
+puts "WebCamp".swapcase
+puts "webcamp".capitalize
+puts "web".capitalize + "camp".capitalize
+puts "webcamp２０１９".tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z')
