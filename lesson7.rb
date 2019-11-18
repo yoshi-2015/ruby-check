8ce31bb324e89c17a22e2a99111eda932151bb6f
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2019-11-18 12:23:48 +0900

    [Finish]chapter2-8

---
+++ lesson7.rb
@@ -0,0 +1,30 @@
+# 1
+# puts "掛け算をします"
+# puts "数字を２つ入力してください"
+#   a=gets.to_i
+#   b=gets.to_i
+#   puts "a*b=#{a*b}"
+# puts "計算終了"
+
+
+# # 2
+# puts "計算をします"
+# puts "何回計算を繰り返しますか？"
+#   a = gets.to_i
+#   b = 1
+#   while b <= a do
+#     puts "#{b}回目の計算開始"
+#     puts "数字を２つ入力してください"
+#       c = gets.to_i
+#       d = gets.to_i
+
+#       puts "c=#{c} d=#{d}"
+#       puts "計算結果"
+#       puts "c+d=#{c+d}"
+#       puts "c-d=#{c-d}"
+#       puts "c*d=#{c*d}"
+#       puts "c/d=#{c/d}"
+
+#       b += 1
+# end
+# puts "計算終了"
